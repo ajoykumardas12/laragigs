9a7d6b60df47360027d64efe0c4e3cde51956016
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2022-05-29 11:45:36 +0530

    edit, delete listings, has bugs

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -29,7 +29,6 @@ class ListingController extends Controller
 
     //store listing data
     public function store(Request $request) {
-        // dd($request->file('logo'));
         $formFields = $request->validate([
             'title' => 'required',
             'company' => 'required',
@@ -47,5 +46,37 @@ class ListingController extends Controller
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing Created Successfully!');
+    }
+
+    //Show edit form
+    public function edit(Listing $listing){
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    //Update listing data
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing Updated Successfully!');
+    }
+
+    //Delete listing
+    public function destroy(Listing $listing) {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,14 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 //Store Listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+//Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+//Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+//Delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 
 //Single Listing
