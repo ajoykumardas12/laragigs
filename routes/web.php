66b261ad8d6d2b0a89907fe4cc3a40d32d63fd3e
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2022-05-29 13:42:11 +0530

    sign up, login, logout

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 
 /*
@@ -34,6 +35,20 @@ Route::put('/listings/{listing}', [ListingController::class, 'update']);
 //Delete listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
-
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Show register/create form
+Route::get('/register', [UserController::class, 'create']);
+
+//Create new user
+Route::post('/users', [UserController::class, 'store']);
+
+//Log user out
+Route::post('/logout', [UserController::class, 'logout']);
+
+//Show login form
+Route::get('/login', [UserController::class, 'login']);
+
+//Log in user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
